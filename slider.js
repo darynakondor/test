8d97999e4ea72348d7a4da3b68f2b4ca30dd0649
Daryna Kondor <condordaryna@gmail.com>
2023-02-21 08:24:06 +0200

    hjbjh

---
+++ slider.js
@@ -0,0 +1,9 @@
+$(".slider").slick({
+    autoplay: true,
+    // infinite: true,
+    autoplaySpeed: 2500,
+    // fade: true,
+    cssEase: 'linear',
+    slidesToShow: 2,
+    slidesToScroll: 1
+})
